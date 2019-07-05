b340f83428b03ff9632e82f0cabd3a61cecda6b6
Jay <Jay@Athena.local>
2019-07-05 10:03:25 +0000

    Limit evaluations to once a day

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -18,21 +18,23 @@ class SurveyController extends Controller
 
     public function store(CreateSurveyRequest $request) // I think were the database info will be going
     {
-        $user = Survey::create($request->all()+['user_id'=>auth()->id()]);
+        // $user = Survey::create($request->all()+['user_id'=>auth()->id()]);
+        //
+        // return redirect('/');
 
-        return redirect('/');
+     $mainUser = Survey::where('user_id', auth()->id())->whereRaw('DATE(created_at) = CURRENT_DATE')->get();
 
-    //  $mainUser = Survey::where('user_id',$user)->whereRaw('DATE(created_at) = CURRENT_DATE')->get();
-    //
-    //
-    // if (count($mainUser) < 1 ){
-    //       Survey::create(['user_id' => $user]);
-    //       session()->flash('message','Evaluation submitted at ' . Carbon::now()->format('H:i:s'));
-    //       return redirect('/');
-    //   }
-    //   else{
-    //     session()->flash('message','You have filled a survey today');
-    //     return redirect()->back();
-    //   }
+
+    if (count($mainUser) < 1 ){
+          // Survey::create(['user_id' => $user]);
+          Survey::create($request->all()+['user_id'=>auth()->id()]);
+
+          session()->flash('message','Evaluation Submitted');
+          return redirect('/');
+      }
+      else{
+        session()->flash('message','You have filled a survey today');
+        return redirect()->back();
+      }
   }
   }
--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -4,10 +4,16 @@
 
 {{-- <div class="container"> --}}
 
+
+
     <div class="row">
       <div class="col-md-3">
       </div>
       <div class="col-md-6">
+
+        @if (session('message'))
+          <h2> {{ session('message') }}</h2>
+        @endif
 
           <form action="/survey" method="post">
             @csrf
